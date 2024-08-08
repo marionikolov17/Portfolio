e319f29fb8ecd4b785e761402ee807d8e1e9cf1c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-08 20:38:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import HeroSection from "./components/HeroSection/HeroSection"
 function App() {
   return (
     <>
-      <main className="w-full absolute min-h-full main-background font-montserrat">
+      <main className="w-full absolute min-h-full main-background font-montserrat overflow-x-hidden">
         <HeroSection />
       </main>
     </>
