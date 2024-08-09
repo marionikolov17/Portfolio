c48de28a66474383f0610783c2384c2726eb0c6d
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-09 13:52:56 +0300

    Import Projects Section in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "./components/HeroSection/HeroSection"
+import ProjectsSection from "./components/ProjectsSection/ProjectsSection"
 
 function App() {
   return (
     <>
       <main className="w-full absolute min-h-full font-montserrat overflow-x-hidden">
         <HeroSection />
+        <ProjectsSection />
       </main>
     </>
   )
