1a1669cdf98d29195f876d5df7cfbd11ee33d9b8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-19 23:59:53 +0300

    Fix project images gallery

--- src/components/ProjectsSection/ProjectCard/DetailCard/DetailCard.jsx
+++ src/components/ProjectsSection/ProjectCard/DetailCard/DetailCard.jsx
@@ -28,13 +28,13 @@ export default function DetailCard({ project }) {
 
     return (
         <>
-            <section className="w-full h-screen fixed project-background top-0 p-24">
-                <div className="w-full min-h-[700px] bg-white rounded-lg flex">
+            <section className="w-full min-h-full fixed project-background top-0 p-24 flex justify-center items-center">
+                <div className="w-full h-[700px] bg-white rounded-lg flex">
                     {/* Project images section */}
-                    <div className="overflow-hidden grow-0 w-[40%] flex items-center justify-center border-r relative">
-                        <img src={project.images[currentImageIndex]} alt="" className="object-cover" />
+                    <div className="overflow-hidden grow-0 w-[40%] h-full flex items-center justify-center border-r relative">
+                        <img src={project.images[currentImageIndex]} alt="" className="object-cover w-full h-full" />
 
-                        <div className="w-full flex absolute bottom-0 py-2 justify-center items-center">
+                        <div className="w-full flex absolute bottom-0 py-2 justify-center items-center bg-white border-t">
                             <FaCaretLeft className="cursor-pointer text-3xl" onClick={goPreviousImage}/>
                             {project.images.map((_, index) => {
                                 if (currentImageIndex == index) {
