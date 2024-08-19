97778e763372819d4b0827f416c3a1b4f975e5ae
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-19 20:35:53 +0300

    Create project card layout

--- src/components/ProjectsSection/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectsSection/ProjectCard/ProjectCard.jsx
@@ -1,3 +1,32 @@
+import { FaPlus, FaGithub } from "react-icons/fa";
+import { MdOutlineWebhook } from "react-icons/md";
+
 export default function ProjectCard() {
-    
+    return (
+        <>
+            <div className="flex flex-col border h-[360px] w-80 rounded-md shadow-sm mx-4 my-4">
+                <div className="w-full overflow-hidden flex justify-center items-center h-28 border-b">
+                    <img src="/images/zynkle-logo-thumbnail.jpg" alt="" />
+                </div>
+                <div className="grow shrink-0">
+                    <h2 className="mt-4 text-center font-bold text-xl">Zynkle</h2>
+                    <p className="text-center px-4 text-sm mt-4">Incoming social media app, connecting people around the world.</p>
+                    <button className="flex items-center mx-auto mt-4 text-sm border rounded-lg py-2 px-4">
+                        See more 
+                        <FaPlus className="ms-2"/>
+                    </button>
+                </div>
+                <div className="grow shrink-1 flex border-t">
+                    <button className="w-1/2 flex items-center justify-center border-r text-base">
+                        Github
+                        <FaGithub className="ms-2"/>
+                    </button>
+                    <button className="w-1/2 flex items-center justify-center text-base">
+                        Demo
+                        <MdOutlineWebhook className="ms-2 text-xl"/>
+                    </button>
+                </div>
+            </div>
+        </>
+    )
 }
--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,5 +1,15 @@
+import ProjectCard from "./ProjectCard/ProjectCard";
+
 export default function ProjectsSection() {
     return (
-        <></>
+        <>
+            <section className="w-full d-block mt-4 mb-4">
+                <h1 className="text-center mt-8 text-4xl font-bold">Projects</h1>
+
+                <div className="w-full flex flex-wrap justify-center mt-6">
+                    <ProjectCard />
+                </div>
+            </section>
+        </>
     )
 }
