f236cfe07748a290efc78ccef89d8ae57e37a5c9
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-19 20:44:11 +0300

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+export const projects = [
+    {
+        id: "1",
+        imageUrl: "/images/zynkle-logo-thumbnail.jpg",
+        name: "Zynkle",
+        summary: "Incoming social media app, connecting people around the world.",
+        githubUrl: "https://github.com/marionikolov17/Zynkle",
+        demoUrl: "https://zynkle-1-gsu7.onrender.com/"
+    },
+    {
+        id: "2",
+        imageUrl: "/images/zynkle-logo-thumbnail.jpg",
+        name: "Fitness-Manager",
+        summary: "Create your workout or choose the best one!",
+        githubUrl: "https://github.com/MihaelPavlov/Fitness-Coaching-Manager",
+        demoUrl: ""
+    },
+]
