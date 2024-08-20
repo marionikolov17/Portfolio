81d18973b0944ee4ef6148b2725b2443e743680f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-20 19:55:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import HeroSection from "./components/HeroSection/HeroSection"
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection"
+import ServicesSection from "./components/ServicesSection/ServicesSection"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <main className="w-full absolute min-h-full font-montserrat overflow-x-hidden">
         <HeroSection />
         <ProjectsSection />
+        <ServicesSection />
       </main>
     </>
   )
