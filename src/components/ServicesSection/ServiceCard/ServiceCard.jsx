840c40678a8138bdbf1aed63672d701af9e92752
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-20 20:23:01 +0300

    Update ServiceCard.jsx

--- src/components/ServicesSection/ServiceCard/ServiceCard.jsx
+++ src/components/ServicesSection/ServiceCard/ServiceCard.jsx
@@ -8,7 +8,7 @@ export default function ServiceCard() {
                     <img src="/images/web-app.png" alt="" className="w-20" />
                 </div>
                 <h3 className="text-center text-xl font-bold">Web Application</h3>
-                <p className="mt-4">Building your desired web application, exactly the way you want! Let's start working together!</p>
+                <p className="mt-4 text-justify">Building your desired web application, exactly the way you want! Let's start working together!</p>
                 <div className="flex justify-center my-4">
                     <button className="flex items-center border rounded-lg py-2 px-4">
                         Apply now
