58338cfeab799cd1c0de5664e48850ab717396dc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-20 13:00:43 +0300

    Update icons.jsx

--- src/data/icons.jsx
+++ src/data/icons.jsx
@@ -17,17 +17,17 @@ import {
 const iconClassName = "text-2xl me-2";
 
 export const icons = {
-  react: <SiReact className={iconClassName} />,
-  vite: <SiVite className={iconClassName} />,
-  redux: <SiRedux className={iconClassName} />,
-  tailwind: <SiTailwindcss className={iconClassName} />,
-  axios: <SiAxios className={iconClassName} />,
-  node: <FaNode className={iconClassName} />,
+  react: <SiReact className={iconClassName + " text-[#61dbfb]"} />,
+  vite: <SiVite className={iconClassName + " text-[#ffc119]"} />,
+  redux: <SiRedux className={iconClassName + " text-[#764abc]"} />,
+  tailwind: <SiTailwindcss className={iconClassName + " text-[#06b6d4]"} />,
+  axios: <SiAxios className={iconClassName + " text-[#6328d8]"} />,
+  node: <FaNode className={iconClassName + " text-[#3c873a]"} />,
   express: <SiExpress className={iconClassName} />,
-  typescript: <SiTypescript className={iconClassName} />,
-  firebase: <SiFirebase className={iconClassName} />,
-  mongodb: <SiMongodb className={iconClassName} />,
-  angular: <SiAngular className={iconClassName} />,
+  typescript: <SiTypescript className={iconClassName + " text-[#007acc]"} />,
+  firebase: <SiFirebase className={iconClassName + " text-[#F6820D]"} />,
+  mongodb: <SiMongodb className={iconClassName + " text-[#00684a]"} />,
+  angular: <SiAngular className={iconClassName + " text-[#dd1b16]"} />,
   mysql: <SiMysql className={iconClassName} />,
-  knex: <SiKnexdotjs className={iconClassName} />,
+  knex: <SiKnexdotjs className={iconClassName + " text-[#e16426]"} />,
 };
