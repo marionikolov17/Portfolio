f8c1a6ed8f58e5eeba0e4f1659927511488d3dfb
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-21 22:59:39 +0300

    Create contact section responsive

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -5,9 +5,9 @@ import { MdOutlineContentPaste, MdOutlineEmail, MdOutlinePhone, MdOutlineSubject
 export default function ContactSection() {
     return (
         <>
-            <section className="w-full flex sm:flex-row mt-24 mb-8">
-                <div className="grow w-1/2 flex justify-end px-24">
-                    <form className="block w-full sm:w-96">
+            <section className="w-full flex flex-col-reverse sm:flex-row mt-10 sm:mt-24 mb-8">
+                <div className="grow w-full sm:w-1/2 flex justify-end px-6 lg:px-24">
+                    <form className="block w-full lg:w-96">
                         <div className="mt-4 mb-6">
                             <label htmlFor="email" className="flex items-center font-medium mb-1">
                                 Your email
@@ -34,7 +34,7 @@ export default function ContactSection() {
                                 className="w-full border-0 rounded-sm ring-inset ring-gray-300 px-2 py-1.5 ring-1 shadow-sm sm:leading-6 focus:ring-2 placeholder:text-gray-400"
                             />
                         </div>
-                        <div className="mb-6">
+                        <div className="mb-3">
                             <label htmlFor="content" className="flex items-center font-medium">
                                 Content
                                 <MdOutlineContentPaste className="ms-2 text-xl"/>
@@ -51,8 +51,8 @@ export default function ContactSection() {
                         </button>
                     </form>
                 </div>
-                <div className="grow-0 w-1/2 block px-6">
-                    <h2 className="text-6xl font-bold">Let's get in touch!</h2>
+                <div className="grow-0 w-full sm:w-1/2 block px-6">
+                    <h2 className="text-3xl sm:text-6xl font-bold">Let's get in touch!</h2>
                     <p className="flex items-center mt-4">
                         <MdOutlineEmail className="me-2 text-xl"/>
                         marionikolovdev@gmail.com
@@ -61,7 +61,7 @@ export default function ContactSection() {
                         <CiLinkedin className="me-2 text-xl"/>
                         <a href="https://www.linkedin.com/in/marionikolovdev/">marionikolovdev</a>
                     </p>
-                    <p className="flex items-center mt-2">
+                    <p className="flex items-center mt-2 mb-4">
                         <MdOutlinePhone className="me-2 text-xl"/>
                         +359884640709
                     </p>
