5bc0405fff82e126a739383c23d84047163e488e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-21 20:48:49 +0300

    Update services.js

--- src/data/services.js
+++ src/data/services.js
@@ -7,5 +7,23 @@ export const services = [
             "Building full-stack application",
             "Fast delivery"
         ]
-    }
+    },
+    {
+        id: "2",
+        imageUrl: "/images/internet.png",
+        name: "Static Website",
+        benefits: [
+            "Building full-stack application",
+            "Fast delivery"
+        ]
+    },
+    {
+        id: "3",
+        imageUrl: "/images/api.png",
+        name: "API Development",
+        benefits: [
+            "Building full-stack application",
+            "Fast delivery"
+        ]
+    },
 ]
