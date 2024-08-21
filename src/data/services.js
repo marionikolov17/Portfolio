c34029fdb8d14b90659419645340062c9039034a
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-21 20:41:48 +0300

    Update services.js

---
+++ src/data/services.js
@@ -0,0 +1,11 @@
+export const services = [
+    {
+        id: "1",
+        imageUrl: "/images/web-app.png",
+        name: "Web Application",
+        benefits: [
+            "Building full-stack application",
+            "Fast delivery"
+        ]
+    }
+]
