fd420ed89e4805f71dd631196342458635625c5b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-22 21:58:34 +0300

    Finish Zynkle project description

--- src/data/projects.js
+++ src/data/projects.js
@@ -22,10 +22,37 @@ export const projects = [
             ]
         },
         features: [
-            "User authentication that uses more complex authentication flow, in order to bring more security."
+            "User authentication that uses more complex authentication flow, in order to bring more security.",
+            "Implementation of infinite scrolling on home page, which brings more discoverability to the users and their posts.",
+            "Creation of posts which uses memory storage for preparing the file, and then saves it to a cloud file storage.",
+            "Interacting with posts - like/dislike, save/unsave, share and comment",
+            "Deletion of post with resource protection (owner only)",
+            "Commenting on a post",
+            "Like/dislike and reply to comments",
+            "Delete comment/s (comment owner or post owner)",
+            "Like/dislike replies",
+            "Delete reply/ies (reply owner or post owner)",
+            "Following and unfollowing other users",
+            "Editing your profile",
+            "Searching for users, based on their usernames or full names",
+            "Instant user feedback for every action",
+            "Beautiful error and message toasts",
+            "Logging out of the application",
+            "Clean and simple design with consistency and relation to the app's mission and name",
+            "Fully responsive design",
         ],
         skills: [
-            "Polished my React skills."
+            "Polished my React skills.",
+            "Learned Redux State management.",
+            "Used Feature-Sliced Design pattern in React.",
+            "Learned to handle requests with axios.",
+            "Learned how to implement instant user action feedback and fall states.",
+            "Learned how to deploy Full-stack web application.",
+            "Learned how to upload files to server and to cloud file storage.",
+            "Polished my Express.js skills with implementing Typescript.",
+            "Learned the importance and difference of Development and Production environment.",
+            "Use different servers and databases for Development and Production.",
+            "Became very comfortable with TailwindCSS"
         ]
     },
     {
