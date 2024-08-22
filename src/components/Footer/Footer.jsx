7bb50f64b37628b58704c2240bd7a5d0648b7e23
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-22 12:58:08 +0300

    Create footer for attribution

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,13 @@
 export default function Footer() {
     return (
-        <></>
+        <>
+            <footer className="w-full bg-black max-h-max mt-12 p-4 flex flex-col items-center">
+                <a 
+                    href="https://www.flaticon.com/free-icons/api" 
+                    className="text-white text-center text-base sm:text-xl font-medium" 
+                    title="icons"
+                >Icons created by Freepik - Flaticon</a>
+            </footer>
+        </>
     )
 }
