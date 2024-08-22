e642f518d6a781b6a1e3814300022949e17aa0da
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-22 22:30:40 +0300

    Update testimonials.js

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,1 +1,9 @@
-export const testimonials = []
+export const testimonials = [
+    {
+        id: "1",
+        text: "Mario is a great employee, and get everything done fast.",
+        authorImageUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png",
+        authorName: "Mario Nikolov",
+        authorJob: "Senior Javascript Developer"
+    }
+]
