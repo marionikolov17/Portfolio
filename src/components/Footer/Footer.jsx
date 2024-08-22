7d195fcac8791d4ec52e295bf70286adb40fdc55
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-22 13:15:55 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,13 @@
 export default function Footer() {
     return (
         <>
-            <footer className="w-full bg-black max-h-max mt-12 p-4 flex flex-col items-center">
+            <footer className="w-full bg-black max-h-max mt-12 py-3 flex flex-col items-center">
                 <a 
                     href="https://www.flaticon.com/free-icons/api" 
                     className="text-white text-center text-base sm:text-xl font-medium" 
                     title="icons"
                 >Icons created by Freepik - Flaticon</a>
+                <p className="text-white text-sm">All rights reserved. 2024©</p>
             </footer>
         </>
     )
