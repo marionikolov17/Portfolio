e3dad08b4f5d03a1438656cde2223241db7acdcc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-23 14:04:39 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <>
             <footer className="z-50 w-full bg-black max-h-max mt-12 py-3 flex flex-col items-center">
                 <a 
-                    href="https://www.flaticon.com/free-icons/api" 
+                    href="https://www.flaticon.com" 
                     className="text-white text-center text-base sm:text-xl font-medium" 
                     title="icons"
                 >Icons created by Freepik - Flaticon</a>
