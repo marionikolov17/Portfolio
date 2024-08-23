11aa820cdc3935a3d63aaf1ca55c208798440226
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-23 13:37:34 +0300

    Update desktop navigation layout

--- src/components/DesktopNavigation/DesktopNavigation.jsx
+++ src/components/DesktopNavigation/DesktopNavigation.jsx
@@ -1,7 +1,8 @@
 export default function DesktopNavigation() {
     return (
         <>
-            <nav className="fixed bg-white h-screen left-0 w-[68px] z-40 shadow-sm border-r hidden lg:flex flex-col items-center overflow-hidden">
+        <section className="fixed left-0 min-h-screen z-40 hidden lg:flex items-center">
+            <nav className="bg-white w-[68px] shadow-md border flex flex-col items-center overflow-hidden">
                 <div className="mt-4 rounded-full border p-2 shadow-sm">
                     <h3 className="flex items-end text-lg font-bold">M
                         <div className="w-2 h-2 bg-green-500 rounded-full mb-1"></div>
@@ -20,6 +21,7 @@ export default function DesktopNavigation() {
                     <a href="" className="text-lg font-normal inline-block rotate-90">Contact</a>
                 </div>
             </nav>
+        </section>
         </>
     )
 }
