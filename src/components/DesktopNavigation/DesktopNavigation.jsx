2cd0f2fd1fbfe1eda5fe22fc35d7153da9e8f8d0
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-23 13:44:45 +0300

    Make scroll to top function

--- src/components/DesktopNavigation/DesktopNavigation.jsx
+++ src/components/DesktopNavigation/DesktopNavigation.jsx
@@ -1,9 +1,13 @@
 export default function DesktopNavigation() {
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    }
+
     return (
         <>
         <section className="fixed left-0 min-h-screen z-40 hidden lg:flex items-center">
             <nav className="bg-white w-[68px] shadow-md border flex flex-col items-center overflow-hidden">
-                <div className="mt-4 rounded-full border p-2 shadow-sm cursor-pointer">
+                <div className="mt-4 rounded-full border p-2 shadow-sm cursor-pointer" onClick={scrollToTop}>
                     <h3 className="flex items-end text-lg font-bold">M
                         <div className="w-2 h-2 bg-green-500 rounded-full mb-1"></div>
                     </h3>
