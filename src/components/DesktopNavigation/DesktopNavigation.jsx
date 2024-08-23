d467750643b4b92a428ade34665562c7c70aed5e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-23 13:58:14 +0300

    Animate desktop navigation on scroll

--- src/components/DesktopNavigation/DesktopNavigation.jsx
+++ src/components/DesktopNavigation/DesktopNavigation.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function DesktopNavigation() {
+    const [offset, setOffset] = useState(0);
+
     const scrollToTop = () => {
         window.scrollTo(0, 0);
     }
+
+    useEffect(() => {
+        const onScroll = () => {
+            console.log(window.scrollY);
+            setOffset(window.scrollY);
+        }
+
+        window.removeEventListener("scroll", onScroll);
+        window.addEventListener("scroll", onScroll);
+
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+        }
+    })
 
     return (
         <>
@@ -12,16 +31,41 @@ export default function DesktopNavigation() {
                         <div className="w-2 h-2 bg-green-500 rounded-full mb-1"></div>
                     </h3>
                 </div>
-                <div className="w-full min-h-28 py-6 flex justify-center items-center mt-6 border-r-4 border-r-green-500 bg-gray-100">
+                <div 
+                    className={
+                        offset > 150 && offset < 900 ?
+                        "w-full min-h-28 py-6 flex justify-center items-center mt-6 border-r-4 border-r-green-500 bg-gray-100"
+                        :
+                        "w-full min-h-28 py-6 flex justify-center items-center mt-6 hover:bg-gray-100"
+                    }
+                >
                     <a href="#projects" className="text-lg font-normal inline-block rotate-90">Projects</a>
                 </div>
-                <div className="w-full min-h-28 py-6 flex justify-center items-center hover:bg-gray-100">
+                <div className={
+                    offset > 900 && offset < 2200 ?
+                    "w-full min-h-28 py-6 flex justify-center items-center border-r-4 border-r-green-500 bg-gray-100"
+                    :
+                    "w-full min-h-28 py-6 flex justify-center items-center hover:bg-gray-100"
+                    }
+                >
                     <a href="#services" className="text-lg font-normal inline-block rotate-90">Services</a>
                 </div>
-                <div className="w-full min-h-28 py-6 flex justify-center items-center hover:bg-gray-100">
+                <div className={
+                    offset > 2700 && offset < 3700 ?
+                    "w-full min-h-28 py-6 flex justify-center items-center border-r-4 border-r-green-500 bg-gray-100"
+                    :
+                    "w-full min-h-28 py-6 flex justify-center items-center hover:bg-gray-100"
+                    }
+                >
                     <a href="#career" className="text-lg font-normal inline-block rotate-90">Career</a>
                 </div>
-                <div className="w-full min-h-28 py-6 flex justify-center items-center hover:bg-gray-100">
+                <div className={
+                    offset > 3700 ?
+                    "w-full min-h-28 py-6 flex justify-center items-center border-r-4 border-r-green-500 bg-gray-100"
+                    :
+                    "w-full min-h-28 py-6 flex justify-center items-center hover:bg-gray-100"
+                    }
+                >
                     <a href="#contact" className="text-lg font-normal inline-block rotate-90">Contact</a>
                 </div>
             </nav>
