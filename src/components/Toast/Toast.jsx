40149d83fb06a78e85b4a98741d5d31ce64dcb76
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-24 15:41:51 +0300

    Update Toast.jsx

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -6,10 +6,10 @@ export default function Toast({ message, setMessage }) {
         <div className="fixed bottom-0 w-full flex justify-center mb-4 z-40">
           <div
             id="toast-default"
-            className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+            className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow"
             role="alert"
           >
-            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
+            <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg">
               <svg
                 className="w-4 h-4"
                 aria-hidden="true"
@@ -30,7 +30,7 @@ export default function Toast({ message, setMessage }) {
             <div className="ms-3 text-sm font-normal">{message}</div>
             <button
               type="button"
-              className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+              className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
               data-dismiss-target="#toast-default"
               aria-label="Close"
               onClick={() => setMessage("")}
