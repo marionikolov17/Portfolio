c92acdd070ea2b546423488246124c6b203009dc
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-24 13:23:52 +0300

    Animate footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,23 @@
+import { motion } from "framer-motion"
+
 export default function Footer() {
     return (
         <>
             <footer className="z-50 w-full bg-black max-h-max mt-12 py-3 flex flex-col items-center">
-                <a 
+                <motion.a 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
                     href="https://www.flaticon.com" 
                     className="text-white text-center text-base sm:text-xl font-medium" 
                     title="icons"
-                >Icons created by Freepik - Flaticon</a>
-                <p className="text-white text-sm">All rights reserved. 2024©</p>
+                >Icons created by Freepik - Flaticon</motion.a>
+                <motion.p 
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className="text-white text-sm"
+                >All rights reserved. 2024©</motion.p>
             </footer>
         </>
     )
