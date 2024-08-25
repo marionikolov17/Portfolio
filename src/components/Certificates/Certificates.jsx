e2ce1d671c1f1d6206c9f46af71f4fff6a3bf24f
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-25 19:14:22 +0300

    Create softuni certificates images

--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,5 +1,9 @@
 export default function Certificates() {
     return (
-        <></>
+        <>
+            <section className="w-full mt-8 flex flex-col items-center max-h-max">
+
+            </section>
+        </>
     )
 }
