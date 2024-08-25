b3775f7328a5abec6c8da510df544bbb1ba7e0c7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-25 20:49:23 +0300

    Display certificate dynamically

--- src/components/Certificates/Certificate/Certificate.jsx
+++ src/components/Certificates/Certificate/Certificate.jsx
@@ -1,21 +1,22 @@
+/* eslint-disable react/prop-types */
 import { FaAngleLeft, FaAngleRight, FaExternalLinkAlt } from "react-icons/fa";
 
-export default function Certificate() {
+export default function Certificate({ certificate }) {
   return (
     <>
       <img
         className="max-w-full sm:max-w-[500px]"
-        src="/images/certificates/softuni-react.jpg"
+        src={certificate.imageUrl}
         alt="certificate"
       />
       <div className="flex items-center justify-around mt-4">
         <FaAngleLeft className="mx-4 text-3xl cursor-pointer text-gray-500 hover:text-black" />
         <div className="grow flex flex-col items-center">
           <h1 className="mt-2 text-xl sm:text-2xl font-bold text-center">
-            ReactJS - June 2024
+            {certificate.title}
           </h1>
           <a
-            href=""
+            href={certificate.credentialUrl}
             className="flex w-max items-center mt-3 border rounded-lg py-2 px-4 ring-1 ring-gray-100 hover:bg-gray-100 hover:text-blue-700"
           >
             Show credential
--- src/components/Certificates/Certificates.jsx
+++ src/components/Certificates/Certificates.jsx
@@ -1,28 +1,11 @@
-import { FaAngleLeft, FaAngleRight, FaExternalLinkAlt } from "react-icons/fa";
+import { certificates } from "../../data/certificates";
+import Certificate from "./Certificate/Certificate";
 
 export default function Certificates() {
     return (
         <>
             <section className="w-full mt-10 flex flex-col items-center max-h-max px-6">
-                <img 
-                    className="max-w-full sm:max-w-[500px]"
-                    src="/images/certificates/softuni-react.jpg" 
-                    alt="certificate" 
-                />
-                <div className="flex items-center justify-around mt-4">
-                    <FaAngleLeft className="mx-4 text-3xl cursor-pointer text-gray-500 hover:text-black"/>
-                    <div className="grow flex flex-col items-center">
-                        <h1 className="mt-2 text-xl sm:text-2xl font-bold text-center">ReactJS - June 2024</h1>
-                        <a 
-                            href=""
-                            className="flex w-max items-center mt-3 border rounded-lg py-2 px-4 ring-1 ring-gray-100 hover:bg-gray-100 hover:text-blue-700"
-                        >
-                            Show credential
-                            <FaExternalLinkAlt className="ms-2"/>
-                        </a>
-                    </div>
-                    <FaAngleRight className="mx-4 text-3xl cursor-pointer text-gray-500 hover:text-black"/>
-                </div>
+                {certificates.map(certificate => <Certificate key={certificate.id} certificate={certificate}/>)}
             </section>
         </>
     )
