06a730ce0aab9d3cbcc2e888faaf0d267b21f0ab
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-25 20:43:09 +0300

    Create certificates.js

---
+++ src/data/certificates.js
@@ -0,0 +1,8 @@
+export const certificates = [
+    {
+        id: "1",
+        title: "ReactJS - June 2024",
+        imageUrl: "images/certificates/softuni-react.jpg",
+        credentialUrl: "https://softuni.bg/certificates/details/223055/52382810"
+    }
+]
