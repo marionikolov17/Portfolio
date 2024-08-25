9faf2598a1749644fe81779d8045376df78ef214
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-25 21:02:49 +0300

    Add loader on certificate image loading

--- src/components/Certificates/Certificate/Certificate.jsx
+++ src/components/Certificates/Certificate/Certificate.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import { FaAngleLeft, FaAngleRight, FaExternalLinkAlt } from "react-icons/fa";
+import Loader from "../../Loader/Loader";
 
 export default function Certificate({ certificate, goNext, goPrevious }) {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
   return (
     <>
+      {isImageLoading &&
+      <div className="flex justify-center items-center">
+        <Loader />  
+      </div>}
       <img
         className="max-w-full sm:max-w-[500px]"
         src={certificate.imageUrl}
         alt="certificate"
+        onLoad={() => setIsImageLoading(false)}
       />
       <div className="flex items-center justify-around mt-4">
         <FaAngleLeft className="mx-4 text-3xl cursor-pointer text-gray-500 hover:text-black" onClick={goPrevious}/>
