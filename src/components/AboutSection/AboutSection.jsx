b63a131128b0ba51323b06ed8f5759317604f87b
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-27 21:45:43 +0300

    Create About section post basic layout

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,5 +1,18 @@
 export default function AboutSection() {
     return (
-        <></>
+        <>
+            <section className="w-full flex justify-center">
+                <div className="w-[70%] h-[650px] border rounded-lg shadow-sm bg-white flex flex-row">
+                    <div className="w-1/2 flex justify-center overflow-hidden border-r">
+                        <img 
+                            className=""
+                            src="https://firebasestorage.googleapis.com/v0/b/zynkle.appspot.com/o/1722873721408-6296EA50-631B-4E58-B887-CEC068F30D39.jpeg?alt=media&token=f5663ef6-88a2-4780-9ee6-04974ca77248" 
+                            alt="" 
+                        />
+                    </div>
+                    <div className="w-1/2 grow"></div>
+                </div>
+            </section>
+        </>
     )
 }
