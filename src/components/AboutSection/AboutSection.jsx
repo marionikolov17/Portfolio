6e69f8d163f256f6ff01bb0b9e9aee57db9fd243
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-27 22:08:48 +0300

    Update AboutSection.jsx

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,3 +1,5 @@
+import { FaHeart } from "react-icons/fa";
+
 export default function AboutSection() {
     return (
         <>
@@ -10,7 +12,27 @@ export default function AboutSection() {
                             alt="" 
                         />
                     </div>
-                    <div className="w-1/2 grow"></div>
+                    <div className="w-1/2 grow flex flex-col">
+                        <div className="block grow overflow-y-scroll no-scrollbar border-b">
+                            <div className="my-4 flex w-full py-2 px-4">
+                                <div className="w-12 h-12 border shadow-sm flex justify-center rounded-full overflow-hidden">
+                                    <img src="/images/mario-2.png" className="object-cover" alt="" />
+                                </div>
+                                <div className="grow ms-4">
+                                    <h3 className="text-base font-bold"><span className="text-green-500">@</span>mario</h3>
+                                    <p className="text-base">This is just a test text.</p>
+                                    <div className="flex mt-1">
+                                        <time className="text-sm text-slate-400">a few seconds ago</time>
+                                        <p className="text-sm ms-4">3 likes</p>
+                                        <p className="text-sm ms-4">24 shares</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <FaHeart className="text-xl text-green-500"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
