23f80c3d728cdbce69514a841671321d9c618a70
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-28 12:38:32 +0300

    Change about section and comment layout

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -1,17 +1,37 @@
+import { MdDone } from "react-icons/md";
 import Comment from "./Comment/Comment";
 
 export default function AboutSection() {
     return (
         <>
-            <section className="w-full flex justify-center">
-                <div className="w-[40%] h-[650px] border rounded-lg shadow-sm bg-white flex flex-row">
-                    <div className="grow flex flex-col">
+            <section className="px-2 sm:px-0 sm:mt-0 mt-4 w-full flex justify-center">
+                <div className="w-full sm:w-[80%] lg:w-[65%] 2xl:w-[45%] h-max pb-4 border rounded-lg shadow-sm bg-white flex flex-row">
+                    <div className="grow flex flex-col pt-2">
                         <div className="block grow overflow-y-scroll no-scrollbar border-b">
-                            <Comment likes={3} shares={24} time={"a few seconds ago"}>
-                                <p className="text-base">2.5+ years of experience building web applications.</p>
+                            <Comment likes={3} shares={24} time={"just now"}>
+                                <p className="text-sm sm:text-base">2.5+ years of experience building web applications.</p>
                             </Comment>
                             <Comment likes={63} shares={29} time={"4 mintues ago"}>
-                                <p className="text-base">2.5+ years of experience building web applications.</p>
+                                <div className="flex items-center">
+                                    <MdDone className="text-xl text-green-500 me-2"/>
+                                    <p className="text-sm sm:text-base font-medium">Passionate</p>
+                                </div>
+                                <div className="flex items-center mt-1">
+                                    <MdDone className="text-xl text-green-500 me-2"/>
+                                    <p className="text-sm sm:text-base font-medium">Motivated</p>
+                                </div>
+                                <div className="flex items-center mt-1">
+                                    <MdDone className="text-xl text-green-500 me-2"/>
+                                    <p className="text-sm sm:text-base font-medium">Disciplined</p>
+                                </div>
+                                <div className="flex items-center mt-1">
+                                    <MdDone className="text-xl text-green-500 me-2"/>
+                                    <p className="text-sm sm:text-base font-medium">Hard Working</p>
+                                </div>
+                                <div className="flex items-center mt-1">
+                                    <MdDone className="text-xl text-green-500 me-2"/>
+                                    <p className="text-sm sm:text-base font-medium">Attention to detail</p>
+                                </div>
                             </Comment>
                         </div>
                     </div>
--- src/components/AboutSection/Comment/Comment.jsx
+++ src/components/AboutSection/Comment/Comment.jsx
@@ -4,19 +4,19 @@ import { FaHeart } from "react-icons/fa";
 export default function Comment({ likes, shares, time, children }) {
   return (
     <>
-      <div className="my-3 flex w-full py-2 px-4">
-        <div className="w-12 h-12 border shadow-sm flex justify-center rounded-full overflow-hidden">
+      <div className="mt-2 flex w-full py-2 px-4">
+        <div className="w-12 h-12 shrink-0 border shadow-sm flex justify-center rounded-full overflow-hidden">
           <img src="/images/mario-2.png" className="object-cover" alt="" />
         </div>
-        <div className="grow ms-4">
+        <div className="grow shrink ms-4">
           <h3 className="text-base font-bold">
             <span className="text-green-500">@</span>mario
           </h3>
           {children}
           <div className="flex mt-1">
-            <time className="text-sm text-slate-400">{time}</time>
-            <p className="text-sm ms-4">{likes} likes</p>
-            <p className="text-sm ms-4">{shares} shares</p>
+            <time className="text-xs sm:text-sm text-slate-400">{time}</time>
+            <p className="text-xs sm:text-sm ms-4">{likes} likes</p>
+            <p className="text-xs sm:text-sm ms-4">{shares} shares</p>
           </div>
         </div>
         <div>
