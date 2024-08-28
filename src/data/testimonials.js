56ec8a119eeafed6cd966083499f7e335133e9f8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-29 00:39:57 +0300

    Update testimonials.js

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,10 +1,10 @@
 export const testimonials = [
     {
         id: "1",
-        text: "Mario is a great employee, and get everything done fast.",
-        authorImageUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png",
+        text: "Mario Nikolov is a highly professional and dedicated lead developer with strong problem-solving skills, excellent communication, and a collaborative approach that significantly enhances project development and team success.",
+        authorImageUrl: "https://media.licdn.com/dms/image/v2/D4D03AQGa3dl8apD6jQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1674635663202?e=1730332800&v=beta&t=uJKVQoiSHm8Wt1RAhWFkJQUekVRE_b-95cxfcgG6wpc",
         authorName: "Mihael Pavlov",
-        authorJob: "Senior Javascript Developer"
+        authorJob: "Senior Developer"
     },
     {
         id: "2",
