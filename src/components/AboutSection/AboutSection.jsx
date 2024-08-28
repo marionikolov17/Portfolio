9e8cde8ae9c38786a04deb21a73138020ccf393c
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-28 12:22:06 +0300

    Update AboutSection.jsx

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -7,7 +7,12 @@ export default function AboutSection() {
                 <div className="w-[40%] h-[650px] border rounded-lg shadow-sm bg-white flex flex-row">
                     <div className="grow flex flex-col">
                         <div className="block grow overflow-y-scroll no-scrollbar border-b">
-                            <Comment />
+                            <Comment likes={3} shares={24} time={"a few seconds ago"}>
+                                <p className="text-base">2.5+ years of experience building web applications.</p>
+                            </Comment>
+                            <Comment likes={63} shares={29} time={"4 mintues ago"}>
+                                <p className="text-base">2.5+ years of experience building web applications.</p>
+                            </Comment>
                         </div>
                     </div>
                 </div>
