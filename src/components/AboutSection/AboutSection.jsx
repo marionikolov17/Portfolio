cb8faa6be4fe99366e5de574bec5e2e75c32d5e7
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-08-28 12:39:03 +0300

    Update AboutSection.jsx

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ export default function AboutSection() {
     return (
         <>
             <section className="px-2 sm:px-0 sm:mt-0 mt-4 w-full flex justify-center">
-                <div className="w-full sm:w-[80%] lg:w-[65%] 2xl:w-[45%] h-max pb-4 border rounded-lg shadow-sm bg-white flex flex-row">
+                <div className="w-full sm:w-[80%] lg:w-[65%] 2xl:w-[45%] h-max border rounded-lg shadow-sm bg-white flex flex-row">
                     <div className="grow flex flex-col pt-2">
                         <div className="block grow overflow-y-scroll no-scrollbar border-b">
                             <Comment likes={3} shares={24} time={"just now"}>
