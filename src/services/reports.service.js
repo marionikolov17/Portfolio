5e9ec7589a9c04d36222750df3230736e3ea630e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-07 23:17:10 +0300

    Create reportInitVisit method

--- src/services/reports.service.js
+++ src/services/reports.service.js
@@ -1,5 +1,30 @@
-export const reportInitialVisit = async () => {
+import { DATABASE_ID, NOTIFICATIONS_COLLECTION_ID, VIEWS_COLLECTION_ID } from "../constants/database.constant";
+import { databases, ID } from "../lib/appwrite";
 
+export const reportInitVisit = async (data) => {
+    // Create record in views database
+    const createdViewsRecord = await databases.createDocument(
+        DATABASE_ID,
+        VIEWS_COLLECTION_ID,
+        ID.unique(),
+        { "viewer": data.ip, "viewedTime": 0 }
+    )
+    // Create record in notifications database
+    const createdNotificationsRecord = await databases.createDocument(
+        DATABASE_ID,
+        NOTIFICATIONS_COLLECTION_ID,
+        ID.unique(),
+        {
+            "type": "visit",
+            "actorIP": data.ip,
+            "actorCountry": data.country_name,
+            "message": "Visited your portfolio for",
+            "totalTime": 0,
+            "isRead": false
+        }
+    )
+
+    return { createdViewsRecord, createdNotificationsRecord }
 }
 
 export const getClientInformation = async () => {
