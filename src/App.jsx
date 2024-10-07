6091aca774b2eafba20f6ed8a8d1df7a02225faf
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-10-07 23:35:10 +0300

    Create records on initial visit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 /* eslint-disable no-unused-vars */
-import AboutSection from "./components/AboutSection/AboutSection"
-import CareerSection from "./components/CareerSection/CareerSection"
-import ContactSection from "./components/ContactSection/ContactSection"
-import DesktopNavigation from "./components/DesktopNavigation/DesktopNavigation"
-import Footer from "./components/Footer/Footer"
-import HeroSection from "./components/HeroSection/HeroSection"
-import MobileNavigation from "./components/MobileNavigation/MobileNavigation"
-import ProjectsSection from "./components/ProjectsSection/ProjectsSection"
-import Testimonials from "./components/Testimonials/Testimonials"
-import { useInitVisit } from "./hooks/useInitVisit"
+import { useState } from "react";
+import AboutSection from "./components/AboutSection/AboutSection";
+import CareerSection from "./components/CareerSection/CareerSection";
+import ContactSection from "./components/ContactSection/ContactSection";
+import DesktopNavigation from "./components/DesktopNavigation/DesktopNavigation";
+import Footer from "./components/Footer/Footer";
+import HeroSection from "./components/HeroSection/HeroSection";
+import MobileNavigation from "./components/MobileNavigation/MobileNavigation";
+import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
+import Testimonials from "./components/Testimonials/Testimonials";
+import { useInitVisit } from "./hooks/useInitVisit";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState();
+  const [createdNotificationID, setCreatedNotificationID] = useState();
+  const [createdViewID, setCreatedViewID] = useState();
 
   // Tracking reports from here
-  useInitVisit();
+  useInitVisit(setFetchedData, setCreatedNotificationID, setCreatedViewID);
 
   return (
     <>
       <main className="w-full absolute min-h-full font-montserrat overflow-x-hidden">
         <div className="w-full py-2 flex justify-center items-center bg-black">
-          <p className="text-white text-sm">Managed by <a className="underline" href="https://portfolio-administrator.onrender.com/">Administrator</a></p>
+          <p className="text-white text-sm">
+            Managed by{" "}
+            <a
+              className="underline"
+              href="https://portfolio-administrator.onrender.com/"
+            >
+              Administrator
+            </a>
+          </p>
         </div>
         <DesktopNavigation />
         <MobileNavigation />
@@ -33,7 +45,7 @@ function App() {
         <div className="fixed top-0 w-full h-full paper-bg -z-10"></div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hooks/useInitVisit.js
+++ src/hooks/useInitVisit.js
@@ -1,16 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
-import { getClientInformation } from "../services/reports.service";
+/* eslint-disable no-unused-vars */
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getClientInformation, reportInitVisit } from "../services/reports.service";
 import { useEffect } from "react";
 
-export function useInitVisit() {
+export function useInitVisit(setFetchedData, setCreatedNotificationID, setCreatedViewID) {
     const { isError, data, error } = useQuery({
         queryKey: ["client", "info"],
         queryFn: getClientInformation
     });
 
+    const { mutate } = useMutation({
+        mutationFn: (data) => reportInitVisit(data),
+        onSuccess: (response) => {
+            console.log(response)
+            setCreatedNotificationID(response.createdNotificationsRecord.$id);
+            setCreatedViewID(response.createdViewsRecord.$id);
+        }
+    })
+
     useEffect(() => {
         if (data !== null && data !== undefined) {
             console.log(data)
+            setFetchedData(data);
+            mutate(data);
         }
-    }, [data])
+    }, [data, setFetchedData, mutate])
 }
