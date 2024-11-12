ca308b943a716f993d11624165fa69d574ef1bb1
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-11-12 22:10:19 +0200

    Create RedirectLoader.jsx

---
+++ src/shared/components/RedirectLoader/RedirectLoader.jsx
@@ -0,0 +1,9 @@
+export default function RedirectLoader() {
+    return (
+        <>
+            <div className="w-full min-h-screen fixed z-[60] redirect-bg flex flex-col justify-center items-center">
+
+            </div>
+        </>
+    )
+}
